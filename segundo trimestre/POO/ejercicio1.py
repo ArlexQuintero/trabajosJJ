c908e6e9a683f4bb6ebe61fd082a90d00faeea4f
ArlexQuintero <arlex05112005@gmail.com>
2026-02-11 10:08:42 -0500

    inicios en POO

---
+++ segundo trimestre/POO/ejercicio1.py
@@ -0,0 +1,11 @@
+# def saludar (nombre, edad):
+#     print(f"hola como tas {nombre}, tienes {edad}")
+
+# saludar("jairo",30)
+
+#funcione que reciva 3 parametrso y lo retorne
+def suma (num1, num2, num3):
+    return num1 + num2 + num3
+resultado = suma(5, 10, 15)
+print(resultado)
+
